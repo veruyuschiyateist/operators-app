5611c2ecfb2fd41947b9a9dcb68e21438c3a3011
veruyuschiyateist <lybomir.pavlick@gmail.com>
2023-12-30 12:23:58 +0200

    add all

--- app/src/main/java/com/nkt/operatorsapp/ui/operator1/FirstOperatorViewModel.kt
+++ app/src/main/java/com/nkt/operatorsapp/ui/operator1/FirstOperatorViewModel.kt
@@ -1,6 +1,8 @@
 package com.nkt.operatorsapp.ui.operator1
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 
-class FirstOperatorViewModel : ViewModel() {
+class FirstOperatorViewModel  : ViewModel() {
+
 }
