6db591f239a35fb2b1e6954f6a60ceab43cfed99
veruyuschiyateist <lybomir.pavlick@gmail.com>
2023-12-30 14:40:18 +0200

    add operator 1 params

--- app/src/main/java/com/nkt/operatorsapp/ui/operator1/FirstOperatorFragment.kt
+++ app/src/main/java/com/nkt/operatorsapp/ui/operator1/FirstOperatorFragment.kt
@@ -1,19 +1,33 @@
 package com.nkt.operatorsapp.ui.operator1
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.nkt.operatorsapp.MainActivity
 import com.nkt.operatorsapp.R
+import com.nkt.operatorsapp.data.RemoteParamsRepository.Companion.PARAM_1
+import com.nkt.operatorsapp.data.RemoteParamsRepository.Companion.PARAM_2
+import com.nkt.operatorsapp.data.RemoteParamsRepository.Companion.PARAM_3
 import com.nkt.operatorsapp.databinding.FragmentFirstOperatorBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 
+@AndroidEntryPoint
 class FirstOperatorFragment : Fragment() {
 
     private lateinit var binding: FragmentFirstOperatorBinding
+
+    private val viewModel by viewModels<FirstOperatorViewModel>()
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -21,17 +35,32 @@ class FirstOperatorFragment : Fragment() {
         (requireActivity() as MainActivity).binding.topAppBar.setTitle(R.string.operator_1)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
+    @SuppressLint("UnsafeRepeatOnLifecycleDetector")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val view = inflater.inflate(R.layout.fragment_first_operator, container, false)
         binding = FragmentFirstOperatorBinding.bind(view)
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.CREATED) {
+                viewModel.uiState.collect { state ->
+                    when (state) {
+                        FirstOperatorViewModel.UiState.Loading -> {}
+                        is FirstOperatorViewModel.UiState.Loaded -> {
+                            val params = state.params
+
+                            with(binding) {
+                                param1Text.text = params[PARAM_1]
+                                param2Text.text = params[PARAM_2]
+                                param3Text.text = params[PARAM_3]
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
         return binding.root
     }
--- app/src/main/java/com/nkt/operatorsapp/ui/operator1/FirstOperatorViewModel.kt
+++ app/src/main/java/com/nkt/operatorsapp/ui/operator1/FirstOperatorViewModel.kt
@@ -1,8 +1,32 @@
 package com.nkt.operatorsapp.ui.operator1
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nkt.operatorsapp.data.repositories.ParamsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class FirstOperatorViewModel  : ViewModel() {
+@HiltViewModel
+class FirstOperatorViewModel @Inject constructor(
+    private val paramsRepository: ParamsRepository
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow<UiState>(UiState.Loading)
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            val params = paramsRepository.getAll()
+            _uiState.emit(value = UiState.Loaded(params = params))
+        }
+    }
+
+    sealed interface UiState {
+        object Loading : UiState
+        data class Loaded(val params: Map<String, String>) : UiState
+    }
 
 }
